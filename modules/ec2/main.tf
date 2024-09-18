e2b3a51a0d1fbfc06bee411f1e53337456073040
Bryce171717 <brice.jamain17@gmail.com>
2024-09-18 18:40:07 +0200

    Envoi ip publique

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Aws_ec2_01" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-   associate_public_ip_address = true
+  associate_public_ip_address = true
 
   tags = {
     Name = var.instance_name
